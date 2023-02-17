e860445ddd35538734ab69832af057bc9a2e1cc5
ahmed <asosnses@gmail.com>
2023-02-17 15:56:17 -0800

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n <= 89 ; n++)
+	{
+		if (n <= 9)
+		{
+			putchar('0');
+			putchar(n + '0');
+		}
+
+		if (n > 9)
+			putchar(n + '0');
+		putchar(',');
+		putchar(32);	
+	}
+	return (0);
+}
