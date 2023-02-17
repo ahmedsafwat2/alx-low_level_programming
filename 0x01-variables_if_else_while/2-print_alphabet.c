b9edca686437a7e7ceb19d20cf53a0b6b7d132de
ahmed <asosnses@gmail.com>
2023-02-17 11:54:31 -0800

    3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * return always 0 (succes)
+ */
+
+int main(void)
+{
+	char a = 'a';
+
+	for (a = 'a';a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	return (0);
+}
