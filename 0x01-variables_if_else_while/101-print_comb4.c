24965b80ffdc01dc0bc89e572e3731e2cab3ca8b
ahmed <asosnses@gmail.com>
2023-02-17 16:20:51 -0800

    101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+	int n;
+	int i;
+	int j;
+
+	for (n = 0; n <= 7; n++)
+	{
+		for (i = n + 1; i <= 8; i++)
+		{
+			for (j = i + 1; j <= 9; j++)
+			{
+				putchar('0' + n);
+				putchar('0' + i);
+				putchar('0' + j);
+				if ((n == 7 && i == 8 && j == 9) != 1)
+				{
+					putchar(',');
+					putchar(32);
+				}
+			}
+		}	
+	}
+	putchar('\n');
+	return (0);
+}
