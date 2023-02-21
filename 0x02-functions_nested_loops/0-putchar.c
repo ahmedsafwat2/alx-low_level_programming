bd449380504c9049a12d1e3aba6d34d098e8b734
ahmed <asosnses@gmail.com>
2023-02-21 12:36:15 -0800

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char *p = "_putchar";
+
+	while(*p)
+	{
+		_putchar(*p);
+		*p++;
+	}
+	_putchar('\n');
+	return (0);
+}
