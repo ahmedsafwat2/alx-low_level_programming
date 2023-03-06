9b5e548c2fecb3ca514d141d6cfe9cfd5bcbdde3
ahmed <asosnses@gmail.com>
2023-03-06 14:44:26 -0800

    2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,16 +7,14 @@
  */
 char *_strchr(char *s, char c)
 {
-	char *f = 0;
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
-			f = &s[i];
-			break;
+			return (&s[i]);
 		}
 	}
-	return (f);
+	return (0);
 }
