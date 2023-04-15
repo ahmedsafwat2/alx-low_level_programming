b41629abd17e575b69b8915d7937d4bec61c6e64
ahmed <asosnses@gmail.com>
2023-04-14 17:00:27 -0700

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,8 +14,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	while (text_content[i])
-		i++;
+	if (text_content != NULL)
+	{
+		while (text_content[i])
+			i++;
+	}
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, i);
 	if (w == -1 || fd == -1)
