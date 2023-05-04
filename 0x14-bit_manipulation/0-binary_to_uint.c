0b07408d74ce708fa6d22450ecdc68c907821d1c
asosnses <asosnses@gmail.com>
2023-05-04 11:53:48 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,10 +7,10 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0, i = 0;
-	int j;
+	unsigned int result = 0, sum = 1;
+	int i = 0, j, h;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 	while (b[i])
 		i++;
@@ -19,9 +19,11 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		if (b[i] == '1')
+		if (b[j] == '1')
 		{
-			result = result + pow(2, (i - j));
+			for (h = 1; h <= (i - j); h++)
+				sum = sum * 2;
+			result = result + sum;
 		}
 	}
 	return (result);
