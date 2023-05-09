047439a4c1f631706e10506c49528850505b9f51
asosnses <asosnses@gmail.com>
2023-05-08 17:17:19 -0700

    2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		while (text_content[i])
 			i++;
 	}
-	fd = open(filename, O_CREAT | O_TRUNC, 0600 | O_RDWR);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, i);
 	if (w == -1 || fd == -1)
 		return (-1);
