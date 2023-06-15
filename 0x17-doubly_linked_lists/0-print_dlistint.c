e63df31d09a6f769a4f17a8e03ceb1b4b910794f
asosnses <asosnses@gmail.com>
2023-06-15 14:15:48 -0700

    first

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,14 +9,12 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	dilistint current;
 	size_t n = 0;
 
-	current = h;
-	while (current != NULL)
+	while (h != NULL)
 	{
-		printf("%d\n", current->n);
-		current = current->next;
+		printf("%d\n", h->n);
+		h = h->next;
 		n++;
 	}
 	return (n);
